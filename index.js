b692200fbc6ce652e8e38abf2f29e60375bb817d
Powar <pramod-balkrushna.powar@capgemini.com>
2022-10-25 17:45:08 +0530

    Initial commit adding all files

---
+++ index.js
@@ -0,0 +1,8 @@
+const bill = document.getElementById("bill");
+const customTip = document.getElementById("tip-percent");
+const peopleCount = document.getElementById("people");
+const reset = document.getElementsByClassName("reset");
+const percent = document.getElementsByClassName("per-button");
+const tipAmount= document.getElementsByClassName("tip-amount");
+const totalTip = document.getElementsByClassName("total-tip-amount")
+
